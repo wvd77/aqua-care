2c44a6183bedf6ea1174364f50c7b7f195d90480
wvd77 <119440518+wvd77@users.noreply.github.com>
2026-09-21 00:15:33 +0200

    Force PWA refresh for Aqua Care 1.1.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aqua-care-v1.1';
+const CACHE='aqua-care-v1.1.2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon.svg'];
 
 self.addEventListener('install',e=>{
