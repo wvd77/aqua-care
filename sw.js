3509103d5cc8384d706e250996216a5a8ec6d4c4
wvd77 <119440518+wvd77@users.noreply.github.com>
2026-09-21 19:39:37 +0200

    Refresh cache for aquarium layout update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aqua-care-v1.2.2';
+const CACHE='aqua-care-v1.2.3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon.svg'];
 
 self.addEventListener('install',e=>{
