b4e7cbca2d8a20b1183bbb3a2a45d3bbd0c422e8
wvd77 <119440518+wvd77@users.noreply.github.com>
2026-09-21 19:13:37 +0200

    Refresh cache for Aqua Care 1.2.1 UI fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aqua-care-v1.2';
+const CACHE='aqua-care-v1.2.1';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon.svg'];
 
 self.addEventListener('install',e=>{
