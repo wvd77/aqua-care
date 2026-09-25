715858cdfcfa8a032a7ba5518ab588d26b84d3e1
wvd77 <119440518+wvd77@users.noreply.github.com>
2026-09-25 08:16:07 +0200

    Refresh cache for photo preview

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aqua-care-v1.2.3';
+const CACHE='aqua-care-v1.2.4';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon.svg'];
 
 self.addEventListener('install',e=>{
